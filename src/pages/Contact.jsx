cec022b7e4d8821396657a196932c85062c4f813
Padmanaban <padmanaban870@gmail.com>
2024-12-13 12:37:31 +0530

    contact page ui changed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
   }
   return <>
 
-    <div className="px-6 py-24 sm:py-32 lg:px-8">
+    <div className="px-6 py-24 sm:py-20 lg:px-8">
       <div
         className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
       >
