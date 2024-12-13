f02c388bd6bc4faf641c9c17f1c7f2430d951438
Padmanaban <padmanaban870@gmail.com>
2024-12-13 11:24:07 +0530

    backend removed

--- BE/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import sendMesssage from "../controller/sendmessage.js";
-
-const Routes = Router()
-
-Routes.post('/sendMesssage',sendMesssage)
-
-export default Routes
