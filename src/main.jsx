24b96c667026a883f8d567625e0f90a44a827b13
Padmanaban M <160383135+Pad-coder@users.noreply.github.com>
2026-06-15 11:00:06 +0530

    Add 404 route for page not found

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="mini-projects" element={<MiniProjects />} />
+          <Route path="*" element={<div className="h-screen flex items-center justify-center text-white bg-[#0a0a0a]"><h1 className="text-4xl">404 | Page Not Found</h1></div>} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
